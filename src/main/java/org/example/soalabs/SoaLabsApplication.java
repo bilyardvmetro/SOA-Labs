05282cbfa9b534ec9b833f988b1658e4be354044
bilyardvmetro <polenovkirill@gmail.com>
2026-09-10 14:57:58 +0300

    initial commit

---
+++ src/main/java/org/example/soalabs/SoaLabsApplication.java
@@ -0,0 +1,13 @@
+package org.example.soalabs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoaLabsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SoaLabsApplication.class, args);
+    }
+
+}
