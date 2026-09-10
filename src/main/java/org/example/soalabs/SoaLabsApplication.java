ef4c130eaf1ac0082084c7445d7d68dfdb5e1810
yusupovalisaa <yusupovalisa774@mail.ru>
2026-09-10 21:22:53 +0300

    service-2

--- src/main/java/org/example/soalabs/SoaLabsApplication.java
+++ src/main/java/org/example/soalabs/SoaLabsApplication.java
@@ -2,8 +2,7 @@ package org.example.soalabs;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
+@SpringBootApplication(excludeName = {"org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration", "org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration"})
 public class SoaLabsApplication {
 
     public static void main(String[] args) {
